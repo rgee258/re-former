fa1aa4aff1a04b3c4ab753c27aa4d064c05b2e49
Russell <rgee258@gmail.com>
2019-01-02 02:21:41 -0500

    Completed objectives

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { in: 4 .. 20 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4 .. 20 }
   validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
